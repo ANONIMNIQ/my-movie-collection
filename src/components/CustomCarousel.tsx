1f43fda95dd5b276927d646fb81de3dddebd0d85
GEORGI PEHLIVANOV <georgipehlivanov@GEORGIs-Mac-mini.local>
2025-08-09 19:43:04 +0300

    [dyad] Centered carousel for better spacing - wrote 1 file(s)

--- src/components/CustomCarousel.tsx
+++ src/components/CustomCarousel.tsx
@@ -15,7 +15,7 @@ interface CustomCarouselProps {
 
 export const CustomCarousel: React.FC<CustomCarouselProps> = ({ title, movies, selectedMovieIds, onSelectMovie }) => {
   const [emblaRef, emblaApi] = useEmblaCarousel({
-    align: 'start',
+    align: 'center',
     loop: false,
     slidesToScroll: 6, // Scroll by a page of 6 movies
   });
@@ -82,7 +82,7 @@ export const CustomCarousel: React.FC<CustomCarouselProps> = ({ title, movies, s
           <ChevronLeft className="h-8 w-8" />
         </Button>
 
-        <div className="embla px-4" ref={emblaRef}>
+        <div className="embla" ref={emblaRef}>
           <div className="embla__container flex gap-4 py-12">
             {movies.map((movie) => (
               <div
