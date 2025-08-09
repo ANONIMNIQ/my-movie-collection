cd7959d2db66ae3ad087c241633dbd9fa5c92d3e
GEORGI PEHLIVANOV <georgipehlivanov@GEORGIs-Mac-mini.local>
2025-08-09 18:29:44 +0300

    [dyad] Made carousel full-width and aligned content - wrote 1 file(s)

--- src/components/CustomCarousel.tsx
+++ src/components/CustomCarousel.tsx
@@ -69,8 +69,8 @@ export const CustomCarousel: React.FC<CustomCarouselProps> = ({ title, movies, s
           <ChevronLeft className="h-8 w-8" />
         </Button>
 
-        <div className="embla container mx-auto" ref={emblaRef}>
-          <div className="embla__container flex gap-4">
+        <div className="embla" ref={emblaRef}>
+          <div className="embla__container flex gap-4 pl-4 pr-4">
             {movies.map((movie) => (
               <div
                 key={movie.id}
