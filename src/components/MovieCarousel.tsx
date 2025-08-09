5baae812a68fd8c352769b297d9fd9c6ead899d9
GEORGI PEHLIVANOV <georgipehlivanov@GEORGIs-Mac-mini.local>
2025-08-09 11:28:01 +0300

    [dyad] Increased carousel padding to prevent clipping of hovered movie cards. - wrote 1 file(s)

--- src/components/MovieCarousel.tsx
+++ src/components/MovieCarousel.tsx
@@ -54,7 +54,7 @@ export const MovieCarousel: React.FC<MovieCarouselProps> = ({ title, movies, sel
         className="w-full overflow-visible"
         setApi={setApi}
       >
-        <CarouselContent className="pl-16 pr-16 overflow-visible py-12">
+        <CarouselContent className="pl-20 pr-20 overflow-visible py-12">
           {movies.map((movie) => {
             return (
               <CarouselItem
