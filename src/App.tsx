a25a20a9b4cd79c0c37e04e799a7c970c2dbd4e3
GEORGI PEHLIVANOV <georgipehlivanov@GEORGIs-Mac-mini.local>
2025-08-10 23:25:11 +0300

    [dyad] Fixed scroll restoration by implementing React Router's data router - wrote 1 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,62 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, ScrollRestoration } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, ScrollRestoration } from "react-router-dom";
 import { SessionContextProvider } from "./contexts/SessionContext";
 import React from "react";
-import AppRoutes from "./components/AppRoutes";
+
+// Import all page components
+import Index from "@/pages/Index";
+import NotFound from "@/pages/NotFound";
+import MovieDetail from "@/pages/MovieDetail";
+import AddMovie from "@/pages/AddMovie";
+import Login from "@/pages/Login";
+import ImportMovies from "@/pages/ImportMovies";
+import EditMovie from "@/pages/EditMovie";
+import ImportRatings from "@/pages/ImportRatings";
 
 const queryClient = new QueryClient();
+
+// Define routes using createBrowserRouter
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <React.Fragment>
+        <ScrollRestoration />
+        <Index />
+      </React.Fragment>
+    ),
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/movie/:id",
+    element: <MovieDetail />,
+  },
+  {
+    path: "/add-movie",
+    element: <AddMovie />,
+  },
+  {
+    path: "/edit-movie/:id",
+    element: <EditMovie />,
+  },
+  {
+    path: "/import-movies",
+    element: <ImportMovies />,
+  },
+  {
+    path: "/import-ratings",
+    element: <ImportRatings />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 const App = () => {
   return (
@@ -15,12 +65,9 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <ScrollRestoration /> {/* Add ScrollRestoration here */}
-          <SessionContextProvider>
-            <AppRoutes />
-          </SessionContextProvider>
-        </BrowserRouter>
+        <SessionContextProvider>
+          <RouterProvider router={router} />
+        </SessionContextProvider>
       </TooltipProvider>
     </QueryClientProvider>
   );
