f07b8ebe33e6479496e17508815b3ccb325868c0
GEORGI PEHLIVANOV <georgipehlivanov@GEORGIs-Mac-mini.local>
2025-08-10 11:17:00 +0300

    [dyad] Fixed movie card hover overflow on mobile grid - wrote 1 file(s)

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,6 +1,7 @@
 import { Movie } from "@/data/movies";
 import { LazyMovieCard } from "./LazyMovieCard";
 import React from "react";
+import { cn } from "@/lib/utils";
 
 interface MovieGridProps {
   movies: Movie[];
@@ -10,12 +11,18 @@ interface MovieGridProps {
 
 export const MovieGrid = ({ movies, selectedMovieIds, onSelectMovie }: MovieGridProps) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 pb-12 px-6"> {/* Added px-6 */}
+    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 pb-12 px-6">
       {movies.map((movie) => {
         return (
           <div
             key={movie.id}
-            className="relative overflow-visible group/slide"
+            // On small screens (below sm breakpoint), limit the card's base width
+            // and center it, so that when it scales on hover, it stays within view.
+            // On sm and larger screens, it will revert to grid's default sizing.
+            className={cn(
+              "relative overflow-visible group/slide",
+              "max-w-[190px] mx-auto sm:max-w-none sm:mx-0"
+            )}
           >
             <LazyMovieCard
               movie={movie}
