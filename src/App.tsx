430e8628be09c23c602ea8cad42f50e6715c3113
GEORGI PEHLIVANOV <georgipehlivanov@GEORGIs-Mac-mini.local>
2025-08-10 20:32:51 +0300

    [dyad] Added animated page transitions - wrote 5 file(s), added framer-motion package(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import MovieDetail from "./pages/MovieDetail";
@@ -13,8 +13,28 @@ import EditMovie from "./pages/EditMovie";
 import ImportRatings from "./pages/ImportRatings";
 import { SessionContextProvider } from "./contexts/SessionContext";
 import React from "react";
+import { AnimatePresence } from "framer-motion";
+import PageTransition from "./components/PageTransition";
 
 const queryClient = new QueryClient();
+
+const AppRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageTransition><Index /></PageTransition>} />
+        <Route path="/login" element={<PageTransition><Login /></PageTransition>} />
+        <Route path="/movie/:id" element={<PageTransition><MovieDetail /></PageTransition>} />
+        <Route path="/add-movie" element={<PageTransition><AddMovie /></PageTransition>} />
+        <Route path="/edit-movie/:id" element={<PageTransition><EditMovie /></PageTransition>} />
+        <Route path="/import-movies" element={<PageTransition><ImportMovies /></PageTransition>} />
+        <Route path="/import-ratings" element={<PageTransition><ImportRatings /></PageTransition>} />
+        <Route path="*" element={<PageTransition><NotFound /></PageTransition>} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 const App = () => {
   return (
@@ -24,16 +44,7 @@ const App = () => {
         <Sonner />
         <BrowserRouter>
           <SessionContextProvider>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/movie/:id" element={<MovieDetail />} />
-              <Route path="/add-movie" element={<AddMovie />} />
-              <Route path="/edit-movie/:id" element={<EditMovie />} />
-              <Route path="/import-movies" element={<ImportMovies />} />
-              <Route path="/import-ratings" element={<ImportRatings />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+            <AppRoutes />
           </SessionContextProvider>
         </BrowserRouter>
       </TooltipProvider>
